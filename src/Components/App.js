2ef1044cbfa22c8ed2898d4826082f7b9ff08014
Lachlan Stibbard Hawkes <lachlan.s-hawkes@xero.com>
2018-05-11 11:41:03 +1200

    Restructure app by adding App.js

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar/NavBar.js";
+
+const App = () => {
+  return (
+    <div>
+      <div>Hello React!</div>
+      <NavBar></NavBar>
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import NavBar from "./Components/NavBar/NavBar.js";
+import App from "./Components/App.js";
 
-const Index = () => {
-return (
-  <div>
-<div>Hello React!</div>
-<NavBar></NavBar>
-</div>
-);
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<App />, document.getElementById("index"));

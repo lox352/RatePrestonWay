74d8aac599c217afcd7b208078f0a49335743ad8
Lachlan Stibbard Hawkes <lachlan.s-hawkes@xero.com>
2018-05-11 15:12:22 +1200

    Sign up page now works (hooray)

--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -1,40 +1,11 @@
-import React, {Component} from "react";
-import {Link} from 'react-router-dom';
+import React from "react";
 
-import * as routes from '../Constants/Routes.js';
+import SignUpForm from "../Components/SignUpForm/SignUpForm.js"
 
 const SignUpPage = () =>
     <div>
         <h1>SignUp</h1>
         <SignUpForm />
     </div>
-
-const INITIAL_STATE = {
-    username: '',
-    email: '',
-    passwordOne: '',
-    passwordTwo: '',
-    error: null,
-  };
-
-class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { ...INITIAL_STATE };
-    }
-
-    onSubmit = (event) => {}
-
-    render() {
-        return (
-            <form onSubmit={this.onSubmit}>
-
-            </form>
-        );
-    }
-}
-
-
 
 export default SignUpPage;

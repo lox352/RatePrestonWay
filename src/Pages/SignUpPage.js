6357381cbdd45683bb5eca71949ec59c64850de1
Lachlan Stibbard Hawkes <lachlan.s-hawkes@xero.com>
2018-05-11 14:20:26 +1200

    introduce the spread operator to babel plugins

--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -9,9 +9,19 @@ const SignUpPage = () =>
         <SignUpForm />
     </div>
 
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+  };
+
 class SignUpForm extends Component {
     constructor(props) {
         super(props);
+
+        this.state = { ...INITIAL_STATE };
     }
 
     onSubmit = (event) => {}

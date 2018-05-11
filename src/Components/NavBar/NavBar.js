20752de143ee8bc8fcafeabe2d4ff3a95cfb8064
Lachlan Stibbard Hawkes <lachlan.s-hawkes@xero.com>
2018-05-11 14:11:35 +1200

    Reformat nav bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,7 @@
 import React from "react";
 import style from "./NavBar.scss"
 
-const NavBar = () => {
-    return <div className={style.test}>This is the nav bar!</div>;
-  };
+const NavBar = () =>
+  <div className={style.test}>This is the nav bar!</div>;
 
 export default NavBar;
